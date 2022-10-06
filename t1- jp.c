0b65de3c76c114580f58be8455f92d42c71ac74f
joaoedro03 <joaopedro.crobalo03@gmail.com>
2022-10-06 10:34:06 +0100

    Create t1- jp.c

---
+++ t1- jp.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+/*
+   incluir livrarias de entrada 
+   e saida de dados 
+*/
+#include <stdio.h>
+
+//função onde é feita a execução do programa 
+int main()
+{
+   // código com identação 
+   // identação - dentro nde cada bloco de código haver alinhamento vertical
+   //imprimir na consola "Hello world"
+    printf("Hello World");
+   //impressão de uma linha ,mudança linha 
+    printf("\n");
+
+    printf("joão robalo");
+    
+    printf("\ncoca-cola/nslbenfica/n/nsemba-samba");
+   //valor de retorno da função main
+   
+   int myNum=15;
+   printf("\n%d",myNum);
+   
+   int x1=59;
+   float x2=54.00;
+   char x3='J';
+   
+   printf("\n %d ultimos dois numeros do cc",x1);
+   
+   printf("\n ultimo %d dois numeros do cc",x1);
+   
+   printf("\n ultimo dois numeros do cc %d",x1);
+   
+    return 0;
+}
